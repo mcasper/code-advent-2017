0847aefbf4cad73b50ff342c86b58949531d669b
Matt Casper <matthewvcasper@gmail.com>
2018-01-16 13:46:08 -0800

    Day 1 part 1

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"Matts-Macbook-Pro.local.com/code-advent-2017/challenge"
+
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "code-advent-2017"
+
+	app.Action = func(c *cli.Context) {
+		var day int
+		if c.NArg() > 0 {
+			num := c.Args().Get(0)
+			i, err := strconv.Atoi(num)
+			if err != nil {
+				fmt.Println("That's not a number!")
+				return
+			} else {
+				day = i
+			}
+		} else {
+			fmt.Println("Requires a challenge number")
+			return
+		}
+
+		if day == 1 {
+			challenge.DayOne()
+		} else {
+			fmt.Println("Haven't gotten this far!")
+			return
+		}
+	}
+
+	app.Run(os.Args)
+}
